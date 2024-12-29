1a82b9a373a20f42a05cb89888c21eabeec7c612
ehsan1381 <ehsan.tc.evol@gmail.com>
2024-12-29 10:36:49 +0330

    NewtonRaphson Implemented, Bisection fixed

---
+++ src/NewtonRaphson.m
@@ -0,0 +1,23 @@
+% PSEUODO CODE ( based on Numerical Analysis - Reza Mokhtari)
+%   INPUT f, f', e, x
+%   OUTPUT x for which |f(x)| < e
+%   1. let n=0
+%   2. x = x - f(x)/f'(x)
+%   3. if |f(x)|<e print x and stop
+%   4. return to step 2
+
+
+function [ Approximation ] = NewtonRaphson(Function, FunctionDerivative, Tolerance, Init)
+  LoopCounter = 1;
+  LOOPLIMIT = 1e3;
+
+  Approximation = Init;
+  while LoopCounter <= LOOPLIMIT
+    if abs(Function(Approximation)) < Tolerance
+      return ;
+    end % if
+    disp(Approximation)
+    Approximation = Approximation - Function(Approximation)/FunctionDerivative(Approximation);
+    LoopCounter = LoopCounter + 1;
+  end % while
+end % function
