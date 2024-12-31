9cd349450f00143659714659285baeb10a80a2fc
ehsan1381 <ehsan.tc.evol@gmail.com>
2024-12-31 16:59:15 +0330

    Made Index persistent This is to prevent having to track it outside of this function

--- src/ThetaProduct.m
+++ src/ThetaProduct.m
@@ -1,4 +1,10 @@
-function NextProduct = ThetaProduct(LastProduct, XArray, Index, StepSize)
+function NextProduct = ThetaProduct(LastProduct, XArray, StepSize)
+  persistent Index;
+  if isempty(Index)
+    Index = 1;
+  end
+
+
   [ ~, Degree ] = size(LastProduct);
   [ ~, Length ] = size(XArray);
 
