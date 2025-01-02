bec92d3ef931f46c4f300777b1f416294a3da4e2
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-01-03 00:01:12 +0330

    mistype fixed in a disp command

--- src/OneOverFactorial.m
+++ src/OneOverFactorial.m
@@ -5,15 +5,15 @@ function [product] = OneOverFactorial(num)
     arguments
         num (1, 1) uint64
     end % arguments
-    
+
     % check feasibility of input given
     % the 177 limit is calculated based on 64bit
     % floating point. For number larger than 177
     % the program division by 178 in the algorithm
     % will cause underflow to zero
     if num > 177
-        disp("Error : Input is too large to calculate 1/n!. It will cause overflow");
-        
+        disp("Error : Input is too large to calculate 1/n!. It will cause underflow");
+
 	% exit the program
 	return ;
     end
@@ -31,6 +31,6 @@ function [product] = OneOverFactorial(num)
 	    index = index + 1;
 
     end % while
-    
-    
+
+
 end % OneOverFactorial
