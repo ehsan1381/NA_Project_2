b6fb6c12aebf162da1f202d51ae6f169a7e1114c
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-01-14 23:53:22 +0330

    variable names changed for better readability

--- src/ThetaProduct.m
+++ src/ThetaProduct.m
@@ -1,13 +1,12 @@
 % Compute numerical value of theta acccording to forward divided difference formula
-function product = ThetaProduct(x, x_n, h, n)
+function product = ThetaProduct(point, lastPoint, stepSize, n)
   if n < 0
     error 'Argument n should be positive'
   elseif n == 0
    product = 1;
+
   else
-    theta = (x-x_n) / h;
+    theta = (point - lastPoint) / stepSize;
     product = prod(theta + [0:n-1]);
-  end
+  end % if
 end % function
-
-
