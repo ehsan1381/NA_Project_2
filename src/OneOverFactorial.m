dc64d5f7cf20baf2626d2d99a5d0c38ee49c162a
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-01-14 23:56:57 +0330

    some small tweaks input argument is now requested to be of type uint8. It can at most be 177 so why use more memory. the previous disp and return is now an error unnecessary comments removed

--- src/OneOverFactorial.m
+++ src/OneOverFactorial.m
@@ -1,35 +1,25 @@
 % This function calculates the 1/n! at for given n
 
 function [product] = OneOverFactorial(num)
-    % check validity of input arguement
     arguments
-        num (1, 1) uint64
+        num (1, 1) uint8
     end % arguments
 
-    % check feasibility of input given
     % the 177 limit is calculated based on 64bit
-    % floating point. For number larger than 177
-    % the program division by 178 in the algorithm
+    % floating point. For numbers larger than 177
+    % the division by 178 in the algorithm
     % will cause underflow to zero
     if num > 177
-        disp("Error : Input is too large to calculate 1/n!. It will cause underflow");
-
-	% exit the program
-	return ;
+        error("Error : Input %s is too large to calculate 1/n!. It will cause underflow", num2str(num));
+        return ;
     end
 
-    % set the product and index variables
     index = 1;
     product = double(1);
 
-    % continouos division algorithm
     while index <= num
-	    % divide product by the index and store
 	    product = product / index;
-
-	    % increment index
 	    index = index + 1;
-
     end % while
 
 
