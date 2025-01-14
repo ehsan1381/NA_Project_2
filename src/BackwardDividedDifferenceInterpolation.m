5e4ac469822f04b6720b23ee8841e13c9f038d88
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-01-15 00:02:44 +0330

    some comments added

--- src/BackwardDividedDifferenceInterpolation.m
+++ src/BackwardDividedDifferenceInterpolation.m
@@ -2,8 +2,12 @@ function Approximation =  BackwardDividedDifferenceInterpolation(XArray, YArray,
   [~, Size] = size(XArray);
   stepSize = abs(XArray(2) - XArray(1));
 
+  % for ease of further computations
   coefficient = @(i)(OneOverFactorial(i) * BackwardDifference(YArray, i, Size));
+
   Approximation = YArray(Size);
+
+  % to avoind having to access the array on every iteration
   XN = XArray(Size);
 
   for i=[1:Size - 1]
