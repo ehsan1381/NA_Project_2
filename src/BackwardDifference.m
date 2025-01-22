537163d6e1a3727a618db9dc72d249d2b8997725
ehsan1381 <ehsan.tc.evol@gmail.com>
2025-01-22 19:16:44 +0330

    branch only for descriptions and their edits

--- src/BackwardDifference.m
+++
@@ -1,9 +0,0 @@
-function NablaKFi = BackwardDifference(FArray, Order, Index)
-  if Order == 0
-    NablaKFi = FArray(Index);
-  elseif Order == 1
-    NablaKFi = FArray(Index) - FArray(Index - 1);
-  else
-    NablaKFi = BackwardDifference(FArray, Order - 1, Index) - BackwardDifference(FArray, Order - 1, Index - 1);
-  end
-end % function
